c98b2771ef752172e386fe42fca0bfe30e1f960f
Ksenia Mikhailovskaya <ksenia0822@gmail.com>
2016-01-20 15:07:29 -0500

    Initializing project complete

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var express = require( 'express' );
 var app = express(); // creates an instance of an express application
 
+app.use('/', function(req,res, next) {
+	console.log('you asked for:' + req.method + req.path)
+	console.log('Response status code is: ' + res.statusCode);
+	next();
+})
+
+
+app.use('/special', function(req,res, next) {
+	console.log('You reached the special area: ' + req.method + req.path);
+	console.log('Response status code is: ' + res.statusCode);
+	next();
+})
+
 app.get('/', function(req, res, next) {
 	res.send("Welcome!");
+
+})
+
+app.get('/news', function(req, res, next) {
+	res.send("Here is a good news");
 
 })
 
