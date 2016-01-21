dbe6691bc496072f2f024bb3727131377823fa22
Ksenia Mikhailovskaya <ksenia0822@gmail.com>
2016-01-21 10:09:16 -0500

    Section 8 done

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-// could use one line instead: var router = require('express').Router();
+// could we use one line instead: var router = require('express').Router();
+
 var tweetBank = require('../tweetBank');
 
 var bodyParser = require('body-parser');
@@ -15,28 +16,26 @@ router.get('/', function (req, res) {
   res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true  } );
 });
 
-router.get('stylesheets/style.css', function(req,res) {
-	res.sendFile('../public/stylesheets/style.css');
-});
+//??? Part 7.5 I don't really understand what this router does. Do we still need to use it if we use express.static?
+// router.get('stylesheets/style.css', function(req,res) {
+// 	res.sendFile('../public/stylesheets/style.css');
+// });
 
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
   var list = tweetBank.find( {name: name} );
-  res.render( 'index', { title: 'Twitter.js - Posts by ', name, tweets: list,showForm: false } );
+  res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: list, showForm: true } );
 });
 
 router.get('/tweets/:id', function(req, res) {
   var uniqueID = parseInt(req.params.id);
-  //console.log("unique ID is ",uniqueID);
   var tweet = tweetBank.find( { unique_id: uniqueID } );
-  // console.log("tweet is",tweet,"and tweet length is",tweet.length);
   res.render('index', { title: 'Single Tweet', tweets: tweet });
 });
 
 router.post('/tweets', function(req, res) {
   var name = req.body.name;
   var text = req.body.text;
-  console.log('name is: ', name, 'text is: ', text)
   tweetBank.add(name, text);
   res.redirect('/');
 });
